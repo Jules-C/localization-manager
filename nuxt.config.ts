2425e29c1f9a4b996790f5c1277458fbdc98e8b5
Jules-C <juliecook328@gmail.com>
2023-02-26 04:42:12 -0500

    test again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,14 +9,15 @@ export default defineNuxtConfig({
     FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
     // Config within public will be also exposed to the client
     public: {
-        FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-        FIRERBASE_AUTH_DOMAIN: process.env.FIRERBASE_AUTH_DOMAIN,
-        FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-        FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-        FIREBASE_MESSAGING_SENDER_ID:
-            process.env.FIREBASE_MESSAGING_SENDER_ID,
-        FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+      FIRERBASE_AUTH_DOMAIN: process.env.FIRERBASE_AUTH_DOMAIN,
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+      FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
     },
-},
-  })
-  
+  },
+  nitro: {
+    preset: "firebase",
+  },
+})
